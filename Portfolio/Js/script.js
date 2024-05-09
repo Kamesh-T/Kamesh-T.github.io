7fa6ba82a68f590c8891d0be563275e5732d12bf
Kameshwaran T <131839992+Kamesh-T@users.noreply.github.com>
2024-05-09 19:21:45 +0530

    initial commit

---
+++ Portfolio/Js/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded',
+function(){   
+        let Today=new Date();
+        year = Today.getFullYear();
+        document.getElementById("copyright").innerText=year;
+        
+        document.getElementById("resume-btn").addEventListener('click',
+            ()=>{
+                window.alert("Resume not yet Attached");    
+            }   
+        )
+           
+    }
+)
+
+
+    let value="I'm Kameshwaran!";
+    let i=0;
+        function typing(){
+            if(i<value.length){
+                document.getElementById("name").innerHTML+=value.charAt(i);
+                i++;
+                setTimeout(typing,100);
+            }
+            else{
+                typingdev();
+            }
+        }
+
+    let dev="Aspiring Web Develeper < / >";
+    let j=0;
+    function typingdev(){
+        if(j<dev.length){
+            document.getElementById('dev').innerHTML+=dev.charAt(j);
+            j++;
+            setTimeout(typingdev,100);
+        }
+    }
+        
+
+    document.addEventListener('contextmenu',(e)=>{
+        e.preventDefault();
+        window.alert("Contextmenu event doesn't work");
+    })
+
+    document.getElementById('up-arr').addEventListener('click',function ()  {
+        window.scrollTo({
+            top:0,
+        })
+    })
